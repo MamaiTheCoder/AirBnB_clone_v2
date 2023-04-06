ebce14a285b2ebd0fecea335cfec670d1dd567fc
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2023-04-06 23:54:14 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""Distributes an archive to the web servers.
-"""
-from datetime import datetime
-from fabric.api import *
-import shlex
+"""A function that generates a .tgz archive contents"""
 import os
-
-
-env.hosts = ["100.25.14.197", "100.25.221.118"]
-env.user = "ubuntu"
-
+from fabric.api import runs_once, local
+from datetime import datetime
 
 def do_deploy(archive_path):
-    """ Deploys """
+    """Deploy the static files to the servers."""
     if not os.path.exists(archive_path):
         return False
+
+    name_of_file = os.path.basename(archive_path)
+    name_of_folder = name_of_file.replace(".tgz", "")
+    path_of_folder = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        put(archive_path, "/tmp/{}".format(name_of_file))
+        run("mkdir -p {}".format(path_of_folder))
+        run("tar -xzf /tmp/{} -C {}".format(name_of_file, path_of_folder))
+        run("rm -rf /tmp/{}".format(name_of_file))
+        run("mv {}web_static/* {}".format(path_of_folder, path_of_folder))
+        run("rm -rf {}web_static".format(path_of_folder))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except:
-        return False
+        run("ln -s {} /data/web_static/current".format(path_of_folder))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
