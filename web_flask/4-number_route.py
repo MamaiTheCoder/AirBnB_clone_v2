ef56675d9d4f6d0f877d766926ab1938488972fa
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2023-04-24 02:23:20 +0300

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,19 +29,21 @@ def c_pg(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/(<text>)')
-@app.route('/python', defaults={'text': 'is cool'})
-def python_page(text):
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
     """ Display “Python ”, followed by the value of the text variable.
-    (replace underscore _ symbols with a space )
+    (replace underscore _ symbols with a space ) 
     """
-    return 'Python {}'.format(text.replace('_', ' '))
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
 
 
-@app.route('/number/<int: n>')
-def num_only(n):
-    """Display “n is a number” only if n is an integer."""
-    return '{}'.format(n)
+@app.route('/number/<int:n>')
+def number(n):
+    """ Display “n is a number” only if n is an integer."""
+    return "%d is a number" % n
 
 
 if __name__ == '__main__':
